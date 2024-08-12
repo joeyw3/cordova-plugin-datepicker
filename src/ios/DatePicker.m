462c5037ba9359275532ee3d4c1032846d6908bd
Joey Klaassen <joey@w3worx.nl>
2024-08-12 17:13:29 +0200

    Fixes

--- src/ios/DatePicker.m
+++ src/ios/DatePicker.m
@@ -217,6 +217,7 @@
   NSString *minuteIntervalString = [options objectForKey:@"minuteInterval"];
   NSInteger minuteInterval = [minuteIntervalString integerValue];
   NSLocale *locale = [[NSLocale alloc] initWithLocaleIdentifier:[options objectForKey:@"locale"]];
+  NSString *styleString = [options objectForKey:@"style"];
 
   
   if (allowOldDates) {
@@ -258,6 +259,28 @@
 
   if (locale) {
     [self.datePicker setLocale:locale];
+  }
+
+  // pereferredDatePickerStyle property as per iOS14 update, for the old style use UIDatePickerStyleWheels
+  if(IsAtLeastiOSVersion(@"14")) {
+    if ([styleString isEqualToString:@"automatic"]) {
+      self.datePicker.preferredDatePickerStyle = UIDatePickerStyleAutomatic;
+    }
+    else if ([styleString isEqualToString:@"compact"]) {
+      self.datePicker.preferredDatePickerStyle = UIDatePickerStyleCompact;
+    } 
+    else if ([styleString isEqualToString:@"inline"]) {
+      self.datePicker.preferredDatePickerStyle = UIDatePickerStyleInline;
+    }
+    else {
+      self.datePicker.preferredDatePickerStyle = UIDatePickerStyleWheels;
+
+      [self.datePicker setValue:[UIColor blackColor] forKey:@"textColor"];
+      [self.datePicker setValue:@(false) forKey:@"highlightsToday"];
+    }
+  } else {
+    [self.datePicker setValue:[UIColor blackColor] forKey:@"textColor"];
+    [self.datePicker setValue:@(false) forKey:@"highlightsToday"];
   }
 }
 
